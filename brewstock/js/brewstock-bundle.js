4f437afd5cc8fe95fc50c75505ef7ca810bae04b
claudewicak01-sudo <claudewicak01@gmail.com>
2026-09-08 17:02:29 +0700

    Update brewstock-bundle.js

--- brewstock/js/brewstock-bundle.js
+++ brewstock/js/brewstock-bundle.js
@@ -3,7 +3,7 @@
  * Ganti dengan nilai dari Supabase Dashboard > Settings > API
  */
 const SUPABASE_URL = 'https://crdabsztgartxbhwxwzm.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyZGFic3p0Z2FydHhiaHd4d3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3NjI2MTIsImV4cCI6MjA2NDMzODYxMn0.6dvdaStjg8Lk2kkxM6tByHm5J7qZYDU9thlmpn8k_fA';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyZGFic3p0Z2FydHhiaHd4d3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3NDU5MjMsImV4cCI6MjEwNDMyMTkyM30.APsYeC9RJwTJY0eHFLBqLZd9dmTW4yxWqTT2UCBHjag';
 
 const APP_CONFIG = {
   name: 'BrewStock',
